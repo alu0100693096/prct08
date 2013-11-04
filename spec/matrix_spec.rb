926cdd1540f0c43d168564638eab5ad08ad5ffbc
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-11-04 17:54:56 +0000

    Añadido conjunto de pruebas para la comprobación de variables.

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -1,5 +1,24 @@
 require 'matrix.rb'
 
 describe Matrix do
+	before :all do
+		@m1 = Matrix.new
+	end
 
+	describe "# Comprobación de variables" do
+		it "Tiene que tener filas" do
+			@m1.should respond_to :rows
+		end
+		it "Tiene que tener columnas" do
+			@m1.should respond_to :columns
+		end
+		it "Tiene que poder modificar las filas" do
+			@m1.rows = 3
+			@m1.rows.should eq(3)
+		end
+		it "Tiene que poder modificar las columnas" do
+			@m1.columns = 3
+			@m1.columns.should eq(3)
+		end
+	end
 end
