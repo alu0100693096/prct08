d85902fff37f2685c9c05966bb91b2f5a8f7a125
Sergio M. Afonso Fumero <alu0100700459@ull.edu.es>
2013-11-04 19:36:29 +0000

    Añadida la funcionalidad para acceder a los elementos de la matriz.

--- lib/matrix.rb
+++ lib/matrix.rb
@@ -1,3 +1,67 @@
 class Matrix
-	attr_accessor :rows, :columns
+	include Enumerable
+	attr_reader :rows, :columns
+
+	def initialize(rows = 0, columns = 0)
+		validate_sizes(rows, columns)
+		@rows, @columns = rows, columns
+		@container = Array.new(rows){ Array.new(columns, nil) }
+	end
+
+	def rows=(value)
+		validate_sizes(value)
+
+		if value > @rows
+			(value - @rows).times { @container << Array.new(columns, nil) }
+		elsif value < @rows
+			@container.slice!(-1, @rows - value)
+		end
+
+		@rows = value
+	end
+
+	def columns=(value)
+		validate_sizes(value)
+
+		if value != @columns
+			@container.each do |x|
+				if value > @columns
+					(value - @columns).times { x << nil }
+				else
+					x.slice!(-1, @columns - value)
+				end
+			end
+		end
+
+		@columns = value
+	end
+
+	def [](row, column = nil)
+		if(column.nil?)
+			@container[row]
+		else
+			@container[row][column]
+		end
+	end
+
+	def []=(row, column, value)
+		@container[row][column] = value
+	end
+
+	# Método que recorre todos los elementos de la matriz, para utilizar el módulo
+	# "Enumerable".
+	def each()
+		@container.each do |x|
+			x.each { |y| yield y }
+		end
+	end
+
+private
+	# Se asegura que los valores pasados están dentro del rango [0, Infinito)
+	# y que son números enteros. En caso contrario lanza una excepción.
+	def validate_sizes(*values)
+		values.each do |x|
+			raise(ArgumentError, "#{x} no es un valor correcto") unless(x.integer? and x >= 0)
+		end
+	end
 end
--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -2,10 +2,10 @@ require 'matrix.rb'
 
 describe Matrix do
 	before :all do
-		@m1 = Matrix.new
+		@m1 = Matrix.new(1, 1)
 	end
 
-	describe "# Comprobación de variables" do
+	describe "# Comprobacion de variables" do
 		it "Tiene que tener filas" do
 			@m1.should respond_to :rows
 		end
@@ -15,10 +15,12 @@ describe Matrix do
 		it "Tiene que poder modificar las filas" do
 			@m1.rows = 3
 			@m1.rows.should eq(3)
+			expect { @m1.rows = -2 }.to raise_error
 		end
 		it "Tiene que poder modificar las columnas" do
 			@m1.columns = 3
 			@m1.columns.should eq(3)
+			expect { @m1.columns = 3.56 }.to raise_error
 		end
 	end
 
@@ -34,7 +36,28 @@ describe Matrix do
 			@m1[0, 0].should eq(13)
 		end
 		it "Debe ser enumerable" do
+			# Comprobamos que haya incluido el módulo Enumerable
 			@m1.class.included_modules.include?(Enumerable).should eq(true)
+
+			# Creamos un contenido y luego utilizamos el método each para recorrer
+			# la matriz y asegurarnos de que se hace en el orden correcto: Primero
+			# filas y después columnas.
+			@m1.rows = 2
+			@m1.columns = 2
+			@m1[0, 0] = 1
+			@m1[0, 1] = 2
+			@m1[1, 0] = 3
+			@m1[1, 1] = 4
+			i, j = 0, 0
+			@m1.each do |x|
+				x.should eq(@m1[i, j])
+				if j == 0
+					j = 1
+				else
+					j = 0
+					i = 1
+				end
+			end
 		end
 	end
 end
