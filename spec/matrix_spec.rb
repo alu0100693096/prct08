ffbd28b1d82934e942e9de9c6aa5463071a6ad97
Laura Fariña <alu0100693096@ull.edu.es>
2013-11-05 15:48:51 +0000

    Prueba de la resta con errores

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -64,26 +64,51 @@ describe Matrix do
 	describe "# Operaciones" do
 		it "Suma de matrices" do
 			# Operandos:
-			@m1.rows = 2
-			@m1.columns = 2
-			@m1[0, 0] = 1
+                        @m1.rows = 2
+                        @m1.columns = 2
+                        @m1[0, 0] = 1
                         @m1[0, 1] = 2
                         @m1[1, 0] = 3
                         @m1[1, 1] = 4
 
-			@m2 = Matrix.new(2,2)
+                        @m2 = Matrix.new(2,2)
                         @m2[0, 0] = 5
-                        @m2[0, 1] = 6
+                        @m2[0, 1] = 1
                         @m2[1, 0] = 7
-                        @m2[1, 1] = 8
+                        @m2[1, 1] = 4
+			#------------
 
 			@m3 = Matrix.new(2,2)
 			@m3 = @m1+@m2
 			@m3[0, 0].should eq(6)
-                        @m3[0, 1].should eq(8)
+                        @m3[0, 1].should eq(3)
                         @m3[1, 0].should eq(10)
-                        @m3[1, 1].should eq(12)
+                        @m3[1, 1].should eq(8)
 		end
+		it "Resta de matrices" do
+			# Operandos:
+                        @m1.rows = 2
+                        @m1.columns = 2
+                        @m1[0, 0] = 1
+                        @m1[0, 1] = 2
+                        @m1[1, 0] = 3
+                        @m1[1, 1] = 4
+
+                        @m2 = Matrix.new(2,2)
+                        @m2[0, 0] = 5
+                        @m2[0, 1] = 1
+                        @m2[1, 0] = 7
+                        @m2[1, 1] = 4
+			#------------
+
+			@m3 = Matrix.new(2,2)
+                        @m3 = @m1-@m2
+                        @m3[0, 0].should eq(-4)
+                        @m3[0, 1].should eq(1)
+                        @m3[1, 0].should eq(-4)
+                        @m3[1, 1].should eq(0)
+		end
+
 	end
 
 end
